a9f7092c6ea97d446a62b2021679e05d41d6125e
Karolina Segura <karolinaseguradev@gmail.com>
2026-02-20 20:41:43 -0500

    Custom/User configurado correctamente y migraciones limpias

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,3 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'users.User'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ users/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('SUPERADMIN', 'Super Admin'),
+        ('ADMIN', 'Admin'),
+        ('SUPERVISOR', 'Supervisor'),
+        ('CLIENTE', 'Cliente'),
+        ('CONDUCTOR', 'Conductor'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    company_code = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.username} - {self.role}"
